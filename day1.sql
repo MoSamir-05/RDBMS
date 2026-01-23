fc4e1db62e59c3e4f2ec906b9c2c742cf48c8702
MoSamir-05 <ps8949409@gmail.com>
2026-01-23 09:57:31 +0530

    first commit

---
+++ day1.sql
@@ -0,0 +1,18 @@
+-- insert command
+-- insert into em values(1,'samir',21);
+
+-- alter command
+-- alter table em add email varchar(150);
+-- alter table em drop email;
+-- alter table em rename column name to full_name;
+-- alter table em alter column full_name type varchar(200);
+
+-- truncate command
+-- truncate table em;
+-- select * from em;
+
+-- drop command
+-- drop table em;
+
+-- create schema new1;
+-- drop schema new1;
